4dc01f32bc99efddcb63d98c53473e22f0938dce
Simon Porritt <simon@jsplumbtoolkit.com>
2024-05-10 09:50:48 +1000

    remove unused width/height styles in node template

--- angular/src/app/node.component.ts
+++ angular/src/app/node.component.ts
@@ -2,9 +2,8 @@ import {Component} from "@angular/core"
 import {BaseNodeComponent} from "@jsplumbtoolkit/browser-ui-angular"
 
 
-
 @Component({
-  template:`<div style="width:{{obj['w']}}px;height:{{obj['h']}}px;" data-jtk-target="true">
+  template:`<div data-jtk-target="true">
     {{obj['name']}}
     <div class="delete" title="Click to delete" (click)="this.removeNode()"></div>
     <div class="connect" data-jtk-source="true"></div>
